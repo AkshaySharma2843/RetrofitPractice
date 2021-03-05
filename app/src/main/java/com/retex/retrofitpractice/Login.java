3cf20b3842b12ea0a311e1c1c7f2588f58179903
Visitor <akshaysharma2843@gmail.com>
2021-03-05 12:04:19 +0530

    commit

--- app/src/main/java/com/retex/retrofitpractice/Login.java
+++ app/src/main/java/com/retex/retrofitpractice/Login.java
@@ -2,7 +2,10 @@ package com.retex.retrofitpractice;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.text.Html;
 
 public class Login extends AppCompatActivity {
 
@@ -10,5 +13,8 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        getSupportActionBar().setBackgroundDrawable(new ColorDrawable(Color.parseColor("#69C9F5")));
+        getSupportActionBar().setTitle((Html.fromHtml("<font color=\"#000000\">" + "LogIn Menu" + "</font>")));
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 }
